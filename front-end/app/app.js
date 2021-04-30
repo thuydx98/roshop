42f381ef06fd657f8cba725b89682e802914eba8
thuydx98 <thuydx.9598@gmail.com>
2021-05-01 00:13:00 +0700

    Update CI

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -24,7 +24,7 @@ import LanguageProvider from 'containers/LanguageProvider';
 
 // Load the favicon and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
-import '!file-loader?name=[name].[ext]!./images/favicon.ico';
+import '!file-loader?name=[name].[ext]!./assets/images/favicon.ico';
 import 'file-loader?name=.htaccess!./.htaccess';
 /* eslint-enable import/no-unresolved, import/extensions */
 
